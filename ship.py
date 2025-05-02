d7308f0d43a8b53b2417e54e1f1d6405a4b3ca41
Longwei Zhang <longwei2@illinois.edu>
2025-05-01 20:12:30 -0400

    Debug Ship Class: cannot move correctly.

--- ship.py
+++ ship.py
@@ -39,12 +39,12 @@ class Ship():
     def update(self):
         """Update the ship's position based one the movement flag"""
         if self.moving_right:
-            self.rect.x += self.settings.ship_speed
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= self.settings.ship_speed
+            self.x -= self.settings.ship_speed
 
         # Update rect object from self.x
-            self.rect.x = self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current position"""
