e4fb65a93843a522bd94c8787d7350832960de7f
Longwei Zhang <longwei2@illinois.edu>
2025-05-01 22:07:36 -0400

    Add Bullet Class. Initiate Bullets and fire them.

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,7 @@
 import sys
 import pygame
 
+from images.bullet import Bullet
 from settings import Settings
 from ship import Ship
 
@@ -30,12 +31,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -54,6 +57,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_a:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self,event):
         if event.key == pygame.K_d:
@@ -61,10 +66,17 @@ class AlienInvasion:
         elif event.key == pygame.K_a:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create bullet"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
